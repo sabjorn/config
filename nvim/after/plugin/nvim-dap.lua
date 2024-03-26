c3254580cfe5351b45cfe07348083ad65aabe5d2
Steven A. Bjornson <steven@audette.io>
2024-03-26 11:28:12 -0700

    enable dap-ui

--- nvim/after/plugin/nvim-dap.lua
+++ nvim/after/plugin/nvim-dap.lua
@@ -11,59 +11,59 @@ vim.fn.sign_define('DapLogPoint' , { text='◉', texthl='LspDiagnosticsDefaultEr
 vim.fn.sign_define('DapStopped' , { text='➔', texthl='LspDiagnosticsDefaultInformation', linehl='CursorLine' })
 
 -- dap UI
---local ui  = require 'dapui'
---dap.listeners.after['event_initialized']['dapui_config'] = function() ui.open({})
---end
---dap.listeners.before['event_terminated']['dapui_config'] = function()
---	ui.close({})
---end
---
---dap.listeners.before['event_exited']['dapui_config'] = function()
---	ui.close({})
---end
+local ui  = require 'dapui'
+dap.listeners.after['event_initialized']['dapui_config'] = function() ui.open({})
+end
+dap.listeners.before['event_terminated']['dapui_config'] = function()
+	ui.close({})
+end
 
---require 'nvim-dap-virtual-text'.setup {}
+dap.listeners.before['event_exited']['dapui_config'] = function()
+	ui.close({})
+end
 
---ui.setup {
---    mappings = {
---        expand = {'<CR>', '<LeftMouse>'},
---        open = {'o'},
---        remove = {'d', 'x'},
---        edit = {'c'},
---        repl = {'r'},
---    },
---    layouts = {
---        {
---            elements = {
---                'breakpoints',
---                'watches',
---                'stacks',
---                'scopes',
---            },
---            size = 70,
---            position = 'right',
---        }, {
---            elements = {'repl'},
---            size = 20,
---            position = 'bottom',
---        },
---    },
---    floating = {
---        max_height = nil,
---        max_width  = nil,
---    },
---}
+require 'nvim-dap-virtual-text'.setup {}
 
---local function dapui_eval()
---	local expr = vim.fn.input('DAP expression: ')
---	if vim.fn.empty(expr) ~= 0 then
---		return
---	end
---	ui.eval(expr, {})
---end
---
---vim.keymap.set({'n', 'v'}, '<M-k>', ui.eval, {})
---vim.keymap.set('n', '<M-K>', dapui_eval, {})
+ui.setup {
+    mappings = {
+        expand = {'<CR>', '<LeftMouse>'},
+        open = {'o'},
+        remove = {'d', 'x'},
+        edit = {'c'},
+        repl = {'r'},
+    },
+    layouts = {
+        {
+            elements = {
+                'breakpoints',
+                'watches',
+                'stacks',
+                'scopes',
+            },
+            size = 70,
+            position = 'right',
+        }, {
+            elements = {'repl'},
+            size = 20,
+            position = 'bottom',
+        },
+    },
+    floating = {
+        max_height = nil,
+        max_width  = nil,
+    },
+}
+
+local function dapui_eval()
+	local expr = vim.fn.input('DAP expression: ')
+	if vim.fn.empty(expr) ~= 0 then
+		return
+	end
+	ui.eval(expr, {})
+end
+
+vim.keymap.set({'n', 'v'}, '<M-k>', ui.eval, {})
+vim.keymap.set('n', '<M-K>', dapui_eval, {})
 
 -- Python
 require('dap-python').setup('~/micro-services/venv/bin/python')
