4072b71be16cba9bca6eca513d908270f14534ac
Steven A. Bjornson <steven@audette.io>
2024-04-05 11:31:52 -0700

    re-add gitgutter and nvim-web-devicons

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,20 @@
 return {
-    "navarasu/onedark.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('onedark').setup {
-        style = 'dark'
-      }
-      require('onedark').load()
-    end,
+    {
+      'kyazdani42/nvim-web-devicons'
+    },
+    {
+      'airblade/vim-gitgutter',
+    },
+    {
+        "navarasu/onedark.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+          require('onedark').setup {
+            style = 'dark'
+          }
+          require('onedark').load()
+        end,
+    }
   }
---'kyazdani42/nvim-web-devicons',
---'airblade/vim-gitgutter',
 
