9b5e6cced463bbd47f8ed654986ddb0f99471abe
Steven Bjornson <steven@lumen5.com>
2024-10-08 13:20:22 -0700

    setup debugger for TS + Chrome

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -3,30 +3,52 @@ return {
   dependencies = {
     "mfussenegger/nvim-dap-python",
     "rcarriga/nvim-dap-ui",
+    "mxsdev/nvim-dap-vscode-js",
+    {
+      "microsoft/vscode-js-debug",
+      version = "1.x",
+      build = "yarn install && yarn run compile vsDebugServerBundle && mv dist out",  -- Build JS Debugger
+    },
     "tpope/vim-fugitive",
     "nvim-neotest/nvim-nio",
   },
   config = function()
+    local dap, dapui = require("dap"), require("dapui")
+
     require("dapui").setup()
+
     require("dap-python").setup(
       "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      -- vim.fn.getcwd() .. '/venv/bin/python'
     )
-    local dap, dapui = require("dap"), require("dapui")
     require("dap-python").test_runner = "pytest"
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
+    require("dap-vscode-js").setup({
+      debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+    })
+    for _, language in ipairs({ "typescript", "javascript" }) do
+      require("dap").configurations[language] = {
+        {
+          type = "pwa-chrome",
+          name = "Launch Chrome to debug client",
+          request = "launch",
+          url = "http://localhost:8000",
+          sourceMaps = true,
+          protocol = "inspector",
+          port = 9222,
+          webRoot = "${workspaceFolder}",
+          localRoot = "${workspaceFolder}/frontend/js/src",
+          cwd = "${workspaceFolder}",
+          skipFiles = { "**/node_modules/**/*" },
+        },
+      }
     end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
+
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open({ reset = true })
     end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+    dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
     vim.fn.sign_define('DapBreakpoint', { text='●', texthl='LspDiagnosticsDefaultError' })
     vim.fn.sign_define('DapLogPoint' , { text='◉', texthl='LspDiagnosticsDefaultError' })
@@ -36,13 +58,8 @@ return {
     vim.keymap.set('n', '<leader>dc', '<cmd>lua require"dap".continue()<CR>', { desc = '[D]ebug [C]ontinue' })
     vim.keymap.set('n', '<leader>do', '<cmd>lua require"dap".step_over()<CR>', { desc = '[D]ebug Step [O]ver' })
     vim.keymap.set('n', '<leader>di', '<cmd>lua require"dap".step_into()<CR>', { desc = '[D]ebug Step [I]nto' })
-    vim.keymap.set("n", "<Leader>dx", '<cmd>lua require"dap".terminate()<CR>', { desc = '[D]debug E[x]it' })
+    vim.keymap.set("n", "<Leader>dx", '<cmd>lua require"dap".terminate()<CR>', { desc = '[D]ebug E[x]it' })
+
     vim.keymap.set('n', '<leader>dt', '<cmd>lua require"dap-python".test_method()<CR>', { desc = '[D]ebug [T]est' })
-
-
-    vim.keymap.set("n", "<Leader>bp", ":DapToggleBreakpoint<CR>")
-    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
   end,
 }
